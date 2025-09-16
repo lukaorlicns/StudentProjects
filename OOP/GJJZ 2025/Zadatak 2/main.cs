f13be9af08db3312fd7287e145034a22e16763ba
Luka Orlic <138329976+lukaorlicns@users.noreply.github.com>
2025-09-16 16:33:21 +0200

    Update main.cs

--- OOP/GJJZ 2025/Zadatak 2/main.cs
+++ OOP/GJJZ 2025/Zadatak 2/main.cs
@@ -1,1 +1,50 @@
-.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace objektno
+{
+    internal class Program
+    {
+        enum Žanr
+        {
+            Komedija, Akcija, Triler, SF, Horor, Drama
+        }
+        class Film
+        {
+            private string naziv_filma;
+            private string ime_prezime_režisera;
+            private int godina_prikazivanja;
+            private Žanr žanr_filma;
+
+            public string Ime_prezime_režisera
+            {
+                get { return ime_prezime_režisera; }
+                set { ime_prezime_režisera = value; }
+            }
+            public int Godina_prikazivanja
+            {
+                get { return godina_prikazivanja; }
+                set { godina_prikazivanja = value; }
+            }
+            public Žanr Žanr_filma
+            {
+                get { return žanr_filma; }
+                set { žanr_filma = value; }
+            }
+            public string Naziv_filma
+            {
+                get { return naziv_filma; }
+                set { naziv_filma = value; }
+            }
+            
+            
+        }
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
